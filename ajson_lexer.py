77e9f780d987558575ef562bb38e76384ff2a586
santiago <100472051@alumnos.uc3m.es>
2024-02-20 12:02:52 +0100

    delimiter & comparision

--- ajson_lexer.py
+++ ajson_lexer.py
@@ -1,1 +1,76 @@
-print()
+import ply.lex as lex
+import sys
+
+
+reserved = {
+    "TR": "TR",
+    "FL": "FL",
+    "NULL": "NULL"
+}
+
+
+tokens = (
+    "NUMBER",
+    "STRING",
+    "BOOL",
+    "NULL"
+    "COMPARION",
+    "DELIMITER"
+) + reserved
+
+
+def t_NUMBER(token):
+    r'\d'
+    if '.' in token:
+        token.value = float(token.value)
+    else:
+        token.value = int(token.value)
+    return token
+
+
+def t_STRING(token):
+    r'\w'
+    return token
+
+
+def t_BOOL(token):
+    r''
+    return token
+
+
+def t_NULL(token):
+    r''
+    return token
+
+
+def t_COMPARISION(token):
+    r'\=\=|\>\=|\>|\<\=|\<'
+    return token
+
+
+def t_DELIMITER(token):
+    r'\{|\}|\:|,'
+    return token
+
+
+t_ignore = ' \t'
+
+
+def t_newline(token):
+    r'\n+'
+    token.lexer.lineno = token.value.count('\n')
+
+def t_error(token):
+    print("[Exit][Lexer] Illegal character", token.value)
+    token.lexer.skip(1)
+
+
+# build the lexer
+lexer = lex.lex()
+
+
+# execute lexer from a input file
+file = open(sys.argv[1])
+lexer.input(file.read())
+for token in lexer:
+    print(token.type, token.value)
